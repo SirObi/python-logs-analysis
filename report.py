7c320cb093c4dae36b2d6575bf7f658f9addc34a
Obi <leszek.orciuch@gmail.com>
2017-09-04 20:24:05 +0200

    Add shebang, refactor code

--- report.py
+++ report.py
@@ -1,43 +1,44 @@
+#!/usr/bin/python3
 import psycopg2
+
+# Can be reused to connect to database
+def execute_query(query):
+    '''Connects to database, outputs results'''
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
 
 # Generate top articles report
 def get_top_articles():
-    '''Outputs top 3 articles from database'''
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute("select * from top_three_articles")
-    return c.fetchall()
-    db.close()
-
-top_articles = get_top_articles()
-print("Top three articles of all time:")
-for i in range (0, len(top_articles)):
-    print('"{}" - {} views'.format(top_articles[i][0], top_articles[i][1]))
+    '''Prints top 3 articles from database'''
+    query = "select * from top_three_articles"
+    results = execute_query(query)
+    print("Top three articles of all time:")
+    for article, views in results:
+        print('    "{}" - {} views'.format(article, views))
 
 # Generate top authors report
 def get_top_authors():
-    '''Outputs list of top authors from database'''
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute("select * from top_authors")
-    return c.fetchall()
-    db.close()
-
-top_authors = get_top_authors()
-print("\nThe most popular article authors of all time:")
-for i in range (0, len(top_authors)):
-    print("{} - {} views".format(top_authors[i][0], top_authors[i][1]))
+    '''Prints list of top authors from database'''
+    query = "select * from top_authors"
+    results = execute_query(query)
+    print("\nThe most popular article authors of all time:")
+    for author, views in results:
+        print("    {} - {} views".format(author, views))
 
 # Generate client error report
 def get_high_error_days():
     '''Outputs list of days with high request error rate'''
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute("select * from high_error_days")
-    return c.fetchall()
-    db.close()
+    query = "select * from high_error_days"
+    results = execute_query(query)
+    print ("\nDays with error rate > 1%:")
+    for date, percentage in results:
+        print("    {} - {}% errors".format(date, percentage))
 
-high_error_days = get_high_error_days()
-print ("\nDays with error rate > 1%:")
-for i in range (0, len(high_error_days)):
-    print("{} - {}% errors".format(high_error_days[i][0], high_error_days[i][3]))
+if __name__ == "__main__":
+    get_top_articles()
+    get_top_authors()
+    get_high_error_days()
